9870cc89b3ef9fb5118e7bc695e4d08ee8945075
VABG <VABGProductions@hotmail.com>
2019-11-21 00:12:34 +0100

    Improved JavaScript Buttons
    +Buttons now press on down, javascript starts on release (now only requires one press)
    - Buttons keep color from being pressed

---
+++ Assets/OnButtonDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class OnButtonDown : MonoBehaviour, IPointerDownHandler
+{
+    public UnityEvent e;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        e.Invoke();
+        Debug.Log("Hello, this works!");
+    }
+}
